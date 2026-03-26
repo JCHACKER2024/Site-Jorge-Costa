ade346df3c2772e2ee4a474b429ff3cfb1d67ff0
JCHACKER2024 <jorge_1616_costa@hotmail.com>
2026-03-26 11:17:58 +0000

    change js slider

--- JavaScripts/slider.js
+++ JavaScripts/slider.js
@@ -24,28 +24,24 @@ const projectImages = {
 
 // INICIAR SLIDERS
 document.querySelectorAll(".slider").forEach(slider => {
-
     const project = slider.dataset.project;
     const images = projectImages[project];
 
-    if (!images || images.length === 0) return;
-
     let current = 0;
-
     const img = slider.querySelector("img");
+
+    img.src = images[current];
+
     const nextBtn = slider.querySelector(".next");
     const prevBtn = slider.querySelector(".prev");
 
-    // BOTÃO NEXT 
     nextBtn.addEventListener("click", () => {
         current = (current + 1) % images.length;
         img.src = images[current];
     });
 
-    // BOTÃO PREV
     prevBtn.addEventListener("click", () => {
         current = (current - 1 + images.length) % images.length;
         img.src = images[current];
     });
-
 });
